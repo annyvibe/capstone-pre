868da5c8ae4df665d14f0cb9dc71b092f07bfb9b
annyvibe <161568188+annyvibe@users.noreply.github.com>
2025-03-20 18:53:26 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,11 +5,10 @@ let poses = [];
 let trails = new Map();
 let stopButton, continueButton, replayButton;
 let leftHandButton, rightHandButton, leftFootButton, rightFootButton;
+let expandButton, shrinkButton;
 let replaying = false;
 let replayIndex = 0;
-
-let lastRootPosition = null;
-let accumulatedOffset = null;
+let videoExpanded = false; // 是否全屏显示视频
 
 const TRACKED_POINTS = {
     leftHand: 15,
@@ -29,52 +28,64 @@ function gotPoses(results) {
 }
 
 function setup() {
-    createCanvas(640, 360, WEBGL);
+    createCanvas(windowWidth, windowHeight, WEBGL); // 画布全屏
     video = createVideo("assets/test.mp4");
-    video.size(640, 360);
+    video.size(320, 180); // 初始小窗口尺寸
+    video.position(10, 10);
     video.loop();
 
-    stopButton = createButton("stop");
-    stopButton.position(10, 10);
+    stopButton = createButton("Stop");
+    stopButton.position(10, 200);
     stopButton.mousePressed(stopVideo);
 
-    continueButton = createButton("continue");
-    continueButton.position(100, 10);
+    continueButton = createButton("Continue");
+    continueButton.position(70, 200);
     continueButton.mousePressed(continueVideo);
 
     replayButton = createButton("Track Replay");
-    replayButton.position(200, 10);
+    replayButton.position(150, 200);
     replayButton.mousePressed(replayTrack);
 
     leftHandButton = createButton("LH");
-    leftHandButton.position(10, 50);
+    leftHandButton.position(10, 250);
     leftHandButton.mousePressed(() => switchTracking("leftHand"));
 
     rightHandButton = createButton("RH");
-    rightHandButton.position(60, 50);
+    rightHandButton.position(60, 250);
     rightHandButton.mousePressed(() => switchTracking("rightHand"));
 
     leftFootButton = createButton("LF");
-    leftFootButton.position(110, 50);
+    leftFootButton.position(110, 250);
     leftFootButton.mousePressed(() => switchTracking("leftFoot"));
 
     rightFootButton = createButton("RF");
-    rightFootButton.position(160, 50);
+    rightFootButton.position(160, 250);
     rightFootButton.mousePressed(() => switchTracking("rightFoot"));
+
+    expandButton = createButton("Expand Video");
+    expandButton.position(10, 300);
+    expandButton.mousePressed(expandVideo);
+
+    shrinkButton = createButton("Shrink Video");
+    shrinkButton.position(110, 300);
+    shrinkButton.mousePressed(shrinkVideo);
+    shrinkButton.hide(); // 初始隐藏
 
     bodyPose.detectStart(video, gotPoses);
     connections = bodyPose.getSkeleton();
 
     trails.set(TRACKED_POINTS[currentTracking], []);
+}
 
-    accumulatedOffset = createVector(0, 0, 0);
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
 }
 
 function switchTracking(part) {
     currentTracking = part;
-
     trails.clear();
     trails.set(TRACKED_POINTS[currentTracking], []);
+
 }
 
 function stopVideo() {
@@ -88,6 +99,22 @@ function continueVideo() {
 function replayTrack() {
     replaying = true;
     replayIndex = 0;
+}
+
+function expandVideo() {
+    video.size(windowWidth, windowHeight);
+    video.position(0, 0);
+    videoExpanded = true;
+    expandButton.hide();
+    shrinkButton.show();
+}
+
+function shrinkVideo() {
+    video.size(320, 180);
+    video.position(10, 10);
+    videoExpanded = false;
+    shrinkButton.hide();
+    expandButton.show();
 }
 
 function smoothTrail(trail, windowSize) {
@@ -105,62 +132,64 @@ function smoothTrail(trail, windowSize) {
 }
 
 function draw() {
-    scale(height / 2);
-    orbitControl();
-    background(0);
+    if (!videoExpanded) {
+        background(0);
+        scale(height / 2);
+        orbitControl();
 
-    if (poses.length > 0) {
-        let pose = poses[0];
+        if (poses.length > 0) {
+            let pose = poses[0];
 
-        let index = TRACKED_POINTS[currentTracking];
-        let keypoint = pose.keypoints3D[index];
+            let index = TRACKED_POINTS[currentTracking];
+            let keypoint = pose.keypoints3D[index];
 
-        if (keypoint && keypoint.confidence > 0.5) {
-            let trail = trails.get(index);
-            let newPos = createVector(keypoint.x, keypoint.y, keypoint.z);
+            if (keypoint && keypoint.confidence > 0.5) {
+                let trail = trails.get(index);
+                let newPos = createVector(keypoint.x, keypoint.y, keypoint.z);
 
-            if (trail.length === 0 || p5.Vector.dist(trail[trail.length - 1], newPos) > 0.01) {
-                trail.push(newPos);
+                if (trail.length === 0 || p5.Vector.dist(trail[trail.length - 1], newPos) > 0.01) {
+                    trail.push(newPos);
+                }
             }
         }
-    }
 
-    trails.forEach((trail, index) => {
-        let smoothedTrail = smoothTrail(trail, 5);
+        trails.forEach((trail, index) => {
+            let smoothedTrail = smoothTrail(trail, 5);
 
-        strokeWeight(2);
-        noFill();
-        beginShape();
+            strokeWeight(2);
+            noFill();
+            beginShape();
 
-        if (replaying) {
-            if (replayIndex < smoothedTrail.length) {
-                for (let i = 0; i <= replayIndex; i++) {
-                    let pos = smoothedTrail[i];
+            if (replaying) {
+                if (replayIndex < smoothedTrail.length) {
+                    for (let i = 0; i <= replayIndex; i++) {
+                        let pos = smoothedTrail[i];
+                        stroke(255, 150);
+                        vertex(pos.x, pos.y, pos.z);
+                    }
+                    replayIndex++;
+                } else {
+                    replaying = false;
+                    console.log("Replay finished!");
+                }
+            } else {
+                smoothedTrail.forEach(pos => {
                     stroke(255, 150);
                     vertex(pos.x, pos.y, pos.z);
-                }
-                replayIndex++;
-            } else {
-                replaying = false;
-                console.log("Replay finished!");
+                });
             }
-        } else {
-            smoothedTrail.forEach(pos => {
-                stroke(255, 150);
-                vertex(pos.x, pos.y, pos.z);
-            });
-        }
 
-        endShape();
-    });
+            endShape();
+        });
 
-    push();
-    stroke(150, 50);
-    noFill();
-    translate(0, 1);
-    rotateX(PI / 2);
-    plane(3);
-    pop();
+        push();
+        stroke(150, 50);
+        noFill();
+        translate(0, 1);
+        rotateX(PI / 2);
+        plane(3);
+        pop();
+    }
 }
 
 function mousePressed() {
